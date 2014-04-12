73f84897faf7b9390700569cb144b2046237cc2b
Matt Harrison <harrisonm1@ufl.edu>
2014-04-12 17:07:26 -0400

    Deleted FunctionCallable, added some comments to driver and particle

--- src/swarm/pso/Particle.java
+++ src/swarm/pso/Particle.java
@@ -5,21 +5,25 @@ import java.util.Collections;
 import java.util.List;
 
 public class Particle {
-	private List<Double> position;
-	private List<Double> velocity;
-	private List<Double> bestPosition;
+	private List<Double> position; // The current position of the particle
+	private List<Double> velocity; // The amount by which the particle is currently moving in each dimension.
+	private List<Double> bestPosition; // The position this particle has previously visited with the best value.
 	
-	private double value;
-	private double bestValue;
+	private double value; // f(position) or the value of the current position
+	private double bestValue; // f(bestPosition) or the value of the best known position. Should be better than or equal to value.
 	
-	public Particle(List<Double> initialPosition, List<Double> initialVelocity, List<Double> bestPosition, double value, double bestValue) {
-		if (initialPosition.size() != initialVelocity.size()) {
+	public Particle(List<Double> position, List<Double> velocity, List<Double> bestPosition, double value, double bestValue) {
+		if (position.size() == 0 || position.size() != velocity.size()) {
 			throw new IllegalArgumentException("position and velocity of particles must have the same nonzero length");
 		}
-		this.position = new ArrayList<Double>(initialPosition);
-		this.velocity = new ArrayList<Double>(initialVelocity);
+		if (position.size() != velocity.size()) {
+			throw new IllegalArgumentException("position and best position of particles must have the same nonzero length");
+		}
+		//Copy all input values to make structure immutable
+		this.position = new ArrayList<Double>(position);
+		this.velocity = new ArrayList<Double>(velocity);
 		
-		this.bestPosition = new ArrayList<Double>(initialPosition);
+		this.bestPosition = new ArrayList<Double>(position);
 		
 		this.value = value;
 		this.bestValue = bestValue;
--- src/swarm/pso/SwarmDriver.java
+++ src/swarm/pso/SwarmDriver.java
@@ -4,13 +4,17 @@ import java.util.Arrays;
 import java.util.List;
 
 public class SwarmDriver {
+	// SumOfSquares is a sample PSOFunction for testing purposes. It represents a sum-of-squares function with arbitrary dimensions.
+	// For example, a new SumOfSquares(3) represents the function f = x^2 + y^2 + z^2.
 	static class SumOfSquares extends PSOFunction<Double> {
+		// Dimensions is the number of arguments in the function
 		public final int dimensions;
 		
 		public SumOfSquares(int dimensions) {
 			this.dimensions = dimensions;
 		}
 		
+		// function is the function that accepts the list of numbers and returns the sum of their squares
 		@Override
 		public Double function(List<Double> arguments) {
 			if (arguments.size() != getDimensions()) {
@@ -32,15 +36,20 @@ public class SwarmDriver {
 	}
 	
 	public static void main(String[] args) {
+		// function represents f = x^2 + y^2 + z^2
 		PSOFunction<Double> function = new SumOfSquares(3);
 		List<Double> lowerBounds = Arrays.asList(new Double[function.getDimensions()]);
 		List<Double> upperBounds = Arrays.asList(new Double[function.getDimensions()]);
+		// optimize arguments on (-1, 3)
 		for (int i = 0; i < lowerBounds.size(); i++) {
 			lowerBounds.set(i, (double) -1);
 			upperBounds.set(i, (double) 3);
 		}
+		
+		// call the constructor to initialize the optimization class and set up particles
 		SequentialOptimization pso = new SequentialOptimization(function.getDimensions(), function, lowerBounds, upperBounds, 10);
 		
+		// call the optimize method and get the results
 		List<Double> solution = pso.optimize();
 		
 		System.out.println(solution);
