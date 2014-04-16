9e122f0638f541d1b9db4fe15e8dd27dae1b02f8
st3w <stew.reive@gmail.com>
2014-04-16 18:15:55 -0400

    Finished refactoring Sequential Optimization class

--- src/swarm/pso/SequentialOptimization.java
+++ src/swarm/pso/SequentialOptimization.java
@@ -1,8 +1,6 @@
 package swarm.pso;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
@@ -94,148 +92,20 @@ public class SequentialOptimization implements SwarmOptimization {
 		List<Double> selfBestPosition = particles.get(particle).getBestPosition();
 		for (int d = 0; d < swarmConf.getDimensions(); d++) {
 			List<Double> fdrPosition = bestFitnessDistance(particle, d);
-			double dVelocity = inertia * oldVelocity.get(d) + 
-					selfWeight * (selfBestPosition.get(d) - position.get(d)) + 
-					bestWeight * (bestPosition.get(d) - position.get(d)) + 
-					fdrWeight * (fdrPosition.get(d) - position.get(d));
-			velocity.set(d, Math.min(maximumVelocity.get(d), Math.max(-maximumVelocity.get(d), dVelocity)));
+			double dVelocity = swarmConf.getInertia() * oldVelocity.get(d) + 
+					swarmConf.getSelfWeight() * (selfBestPosition.get(d) - position.get(d)) + 
+					swarmConf.getBestWeight() * (bestPosition.get(d) - position.get(d)) + 
+					swarmConf.getFdrWeight() * (fdrPosition.get(d) - position.get(d));
+			velocity.set(d, Math.min(swarmConf.getMaximumVelocity().get(d),
+					Math.max(-swarmConf.getMaximumVelocity().get(d), dVelocity)));
 		}
 		return velocity;
 	}
-}
-/*
-public class SequentialOptimization implements SwarmOptimization {
-	//Optimize function on interval from lower to upper bounds
-	private final int dimensions;
-	private final PSOFunction<Double> function;
-	private final List<Double> lowerBounds;
-	private final List<Double> upperBounds;
-
-	//Use numberOfParticles 
-	private final int numberOfParticles;
 	
-	private final List<Particle> particles;
-	
-	//private final int iterations;
-	private final Double[] mv = {(double) 0.5, (double) 0.5, (double) 0.5};
-	private final List<Double> maximumVelocity = Arrays.asList(mv);
-	private final double inertia = 0.5;
-	private final double selfWeight = 1;
-	private final double bestWeight = 1;
-	private final double fdrWeight = 16;
-	
-	private List<Double> bestPosition = null; // the parameters that give the best known value
-	private double bestValue; // the best known value of the function
-	
-	private final Random rand;
-	
-	public SequentialOptimization(int dimensions, PSOFunction<Double> function, List<Double> lowerBounds, List<Double> upperBounds, int numberOfParticles) {
-		if (dimensions != lowerBounds.size() || dimensions != upperBounds.size()) {
-			throw new IllegalArgumentException("Bounds must have size() = dimensions");
-		}
-		if (dimensions != function.getDimensions()) {
-			throw new IllegalArgumentException("Function must have getDimensions() = dimensions");
-		}
-		
-		this.dimensions = dimensions;
-		this.function = function;
-		this.lowerBounds = Collections.unmodifiableList(new ArrayList<Double>(lowerBounds));
-		this.upperBounds = Collections.unmodifiableList(new ArrayList<Double>(upperBounds));
-		
-		this.numberOfParticles = numberOfParticles;
-
-		particles = Arrays.asList(new Particle[numberOfParticles]);
-
-		rand = new Random();
-		
-		for (int p = 0; p < numberOfParticles; p++) {
-			List<Double> initialPosition = initialPosition();
-			
-			double initialValue = function.function(initialPosition);
-			
-			List<Double> initialVelocity = initialVelocity();
-			
-			particles.set(p, new Particle(initialPosition, initialVelocity, initialPosition, initialValue, initialValue));
-		
-			updateGlobalBest(particles.get(p));
-		}
-	}
-	
-	public List<Double> optimize() {
-		// Perform iterations
-		for (int p = 0; p < numberOfParticles; p++) {
-			System.out.println("Particle " + p + ": " + particles.get(p).getValue());
-		}
-		for (int i = 0; i < 1000; i++) {
-			updateParticleList();
-		}
-		System.out.println("-----------------------------------------------------");
-		for (int p = 0; p < numberOfParticles; p++) {
-			System.out.println("Particle " + p + ": " + particles.get(p).getValue());
-		}
-		return bestPosition;
-	}
-
-	private void updateParticleList() {
-		for (int p = 0; p < numberOfParticles; p++) {
-			updateParticle(p);
-		}
-	}
-	
-	private void updateParticle(int particle) {
-		//System.out.println("Particle " + particle + ", initialPosition: " + particles.get(particle).getPosition());
-		
-		List<Double> velocity = calculateVelocity(particle);
-		List<Double> position = calculatePosition(particle, velocity);
-		List<Double> bestPosition = selectBestPosition(particle, position);
-		
-		//System.out.println("Particle: " + position + ", " + velocity + ", " + function.function(position));
-		particles.set(particle, new Particle(position, velocity, bestPosition, function.function(position), function.function(bestPosition)));
-		updateGlobalBest(particles.get(particle));
-		
-	}
-
-	private List<Double> selectBestPosition(int particle, List<Double> newPosition) {
-		List<Double> currentBestPosition = particles.get(particle).getBestPosition();
-		if (function.function(currentBestPosition) <= function.function(newPosition)) {
-			return currentBestPosition;
-		}
-		else {
-			return newPosition;
-		}
-	}
-
-	private List<Double> calculatePosition(int particle, List<Double> velocity) {
-		List<Double> position = Arrays.asList(new Double[dimensions]);
-		List<Double> oldPosition = particles.get(particle).getPosition();
-		
-		for (int d = 0; d < dimensions; d++) {
-			position.set(d, Math.min(upperBounds.get(d), Math.max(lowerBounds.get(d), oldPosition.get(d) + velocity.get(d))));
-		}
-		
-		return position;
-	}
-
-	private List<Double> calculateVelocity(int particle) {
-		List<Double> velocity = Arrays.asList(new Double[dimensions]);
-		List<Double> oldVelocity = particles.get(particle).getVelocity();
-		List<Double> position = particles.get(particle).getPosition();
-		List<Double> selfBestPosition = particles.get(particle).getBestPosition();
-		for (int d = 0; d < dimensions; d++) {
-			List<Double> fdrPosition = bestFitnessDistance(particle, d);
-			double dVelocity = inertia * oldVelocity.get(d) + 
-					selfWeight * (selfBestPosition.get(d) - position.get(d)) + 
-					bestWeight * (bestPosition.get(d) - position.get(d)) + 
-					fdrWeight * (fdrPosition.get(d) - position.get(d));
-			velocity.set(d, Math.min(maximumVelocity.get(d), Math.max(-maximumVelocity.get(d), dVelocity)));
-		}
-		return velocity;
-	}
-
 	private List<Double> bestFitnessDistance(int particle, int dimension) {
 		List<Double> bestFDRPosition = null;
 		double bestFDR = 0;
-		for (int q = 0; q < numberOfParticles; q++) {
+		for (int q = 0; q < swarmConf.getNumParticles(); q++) {
 			//if (q != particle) {
 				double fdr = fdr(particle, q, dimension);
 				if (bestFDRPosition == null || fdr > bestFDR) {
@@ -246,7 +116,7 @@ public class SequentialOptimization implements SwarmOptimization {
 		}
 		return bestFDRPosition;
 	}
-
+	
 	private double fdr(int currentParticle, int foreignParticle, int dimension) {
 		double fitness = particles.get(currentParticle).getValue() - particles.get(foreignParticle).getBestValue();
 		double distance = Math.abs(
@@ -259,31 +129,31 @@ public class SequentialOptimization implements SwarmOptimization {
 			return Double.MAX_VALUE;
 		}
 	}
-
+	
 	private List<Double> initialPosition() {
-		List<Double> position = Arrays.asList(new Double[dimensions]);
-		for (int d = 0; d < dimensions; d++) {
-			Double lowPos = lowerBounds.get(d);
-			Double highPos = upperBounds.get(d);
+		List<Double> position = Arrays.asList(new Double[swarmConf.getDimensions()]);
+		for (int d = 0; d < swarmConf.getDimensions(); d++) {
+			Double lowPos = swarmConf.getLowerBounds().get(d);
+			Double highPos = swarmConf.getUpperBounds().get(d);
 			position.set(d, lowPos+rand.nextDouble()*(highPos-lowPos));
 		}
 		return position;
 	}
 	
 	private List<Double> initialVelocity() {
-		List<Double> velocity = Arrays.asList(new Double[dimensions]);
-		for (int d = 0; d < dimensions; d++) {
+		List<Double> velocity = Arrays.asList(new Double[swarmConf.getDimensions()]);
+		for (int d = 0; d < swarmConf.getDimensions(); d++) {
 			//Double lowPos = lowerBounds.get(d);
 			//Double highPos = upperBounds.get(d);
 			//Double lowVel = lowPos - highPos;
 			//Double highVel = highPos - lowPos;
-			Double lowVel = -maximumVelocity.get(d);
-			Double highVel = maximumVelocity.get(d);
+			Double lowVel = -swarmConf.getMaximumVelocity().get(d);
+			Double highVel = swarmConf.getMaximumVelocity().get(d);
 			velocity.set(d, lowVel+rand.nextDouble()*(highVel-lowVel));
 		}
 		return velocity;
 	}
-
+	
 	private void updateGlobalBest(Particle p) {
 		if (bestPosition == null || p.getValue() < bestValue) {
 			bestPosition = p.getPosition();
@@ -291,4 +161,3 @@ public class SequentialOptimization implements SwarmOptimization {
 		}
 	}
 }
-*/
