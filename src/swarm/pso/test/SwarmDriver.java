972065b687e47887419c9dc4d5034864360c99ef
st3w <stew.reive@gmail.com>
2014-04-21 00:24:51 -0400

    added command line arguments

--- src/swarm/pso/test/SwarmDriver.java
+++ src/swarm/pso/test/SwarmDriver.java
@@ -39,10 +39,48 @@ public class SwarmDriver {
 	
 	private static Timer timer;
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IllegalArgumentException {
 		// Make a function to optimize
-		PSOFunction<Double> function = new Functions.Rosenbrock(DIMENSIONS);
+		PSOFunction<Double> function;
+		int numParticles = 0;
+		int numIterations = 0;
+		int animationTimeout = 0;
 		
+		if (args.length < 3 && args.length > 0)
+			throw new IllegalArgumentException("You must have either 3 or 4 arguments if any.");
+		else if (args.length >= 3) {
+			int functionNum = Integer.parseInt(args[0]);
+		
+			switch (functionNum) {
+			case 0:
+				function = new Functions.Sphere(DIMENSIONS);
+				break;
+			case 1:
+				function = new Functions.Ackley(DIMENSIONS);
+				break;
+			case 2:
+				function = new Functions.Rosenbrock(DIMENSIONS);
+				break;
+			default:
+				throw new IllegalArgumentException("Function number must be between 0 and 2");
+			}
+			
+			numParticles = Integer.parseInt(args[1]);
+			if (numParticles <= 0) 
+				throw new IllegalArgumentException("Number of particles must be > 0");
+			
+			numIterations = Integer.parseInt(args[2]);
+			if (numIterations <= 0)
+				throw new IllegalArgumentException("Number of iterations must be > 0");
+			
+			if (args.length == 4)
+				animationTimeout = Integer.parseInt(args[3]);
+		} else {
+			function = new Functions.Rosenbrock(DIMENSIONS);
+			numParticles = NUMBER_PARTICLES;
+			numIterations = NUMBER_ITERATIONS;
+			animationTimeout = 0;
+		}
 		// Function bounds are a list of parameters
 		
 		List<Double> maximumVelocity = Arrays.asList(new Double[function.getDimensions()]);
@@ -57,7 +95,7 @@ public class SwarmDriver {
 				function.getLowerBounds(), function.getUpperBounds());
 		
 		SwarmConfiguration swarmConf = new SwarmConfiguration(INITIAL_INERTIA, FINAL_INERTIA, SELF_WEIGHT, BEST_WEIGHT,
-				FDR_WEIGHT,  NUMBER_PARTICLES, NUMBER_ITERATIONS, maximumVelocity, funcConf);
+				FDR_WEIGHT,  numParticles, numIterations, maximumVelocity, funcConf);
 		
 		Random rand;
 		if (USE_SEED) {
@@ -72,7 +110,12 @@ public class SwarmDriver {
 		setupLogPainter(log, swarmConf);
 		
 		SequentialOptimization pso = new SequentialOptimization(swarmConf, rand, log);
-		List<Double> solution = pso.optimize();
+		List<Double> solution;
+		
+		if (animationTimeout > 0) 
+			solution = pso.optimize(animationTimeout);
+		else 
+			solution = pso.optimize();
 		
 		log.writeToFile();
 		
